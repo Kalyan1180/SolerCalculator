f74659067c2c7425733f094c4d911772a6a9ce1e
Kalyan1180 <158819856+Kalyan1180@users.noreply.github.com>
2025-03-24 00:22:15 +0530

    Update batteryData.js

--- src/data/batteryData.js
+++ src/data/batteryData.js
@@ -1,1 +1,8 @@
 
+// src/data/batteryData.js
+export const batteryList = [
+  { name: "Exide Battery 100 AH", energy: 0.96, capacity: 100, price: 8000 },
+  { name: "Exide Battery 150 AH", energy: 1.44, capacity: 150, price: 12000 },
+  { name: "Exide Battery 200 AH", energy: 1.92, capacity: 200, price: 15000 },
+  { name: "Exide Battery 260 AH", energy: 2.49, capacity: 260, price: 24000 },
+];
