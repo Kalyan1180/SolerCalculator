9a238b025ca8210472b04187196eff3103382518
Kalyan1180 <158819856+Kalyan1180@users.noreply.github.com>
2025-03-24 01:11:25 +0530

    Update inverterData.js

--- src/data/inverterData.js
+++ src/data/inverterData.js
@@ -1,7 +1,9 @@
-// src/data/batteryData.js
-export const batteryList = [
-  { name: "Exide Battery 100 AH", energy: 0.96, capacity: 100, price: 8000 },
-  { name: "Exide Battery 150 AH", energy: 1.44, capacity: 150, price: 12000 },
-  { name: "Exide Battery 200 AH", energy: 1.92, capacity: 200, price: 15000 },
-  { name: "Exide Battery 260 AH", energy: 2.49, capacity: 260, price: 24000 },
+// src/data/inverterData.js
+export const inverterList = [
+  { name: "Shamsi Solar Inverter 675 VA/12V", peakLoad: 0.6, maxPanels: 1, batterySupported: 12, cost: 4493 },
+  { name: "UTL Gamma plus 12V 1K", peakLoad: 1, maxPanels: 2, batterySupported: 12, cost: 14342 },
+  { name: "Gamma Plus MPPT Solar Inverter 2600/24 Volt", peakLoad: 2, maxPanels: 4, batterySupported: 24, cost: 18490 },
+  { name: "Gamma Plus MPPT Solar Inverter 3350/24 Volt", peakLoad: 3, maxPanels: 4, batterySupported: 24, cost: 19999 },
+  { name: "Sigma Plus Solar PCU 4kVA/48V", peakLoad: 4, maxPanels: 8, batterySupported: 48, cost: 55887 },
+  { name: "5kVA/48V Alfa+ Solar PCU", peakLoad: 5, maxPanels: 10, batterySupported: 48, cost: 66701 },
 ];
