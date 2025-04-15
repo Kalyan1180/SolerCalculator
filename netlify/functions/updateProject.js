ef26e388a23b0d36c07d0d2108c2f829360f270e
Kalyan Patra <kalyanpatra1180@gmail.com>
2025-04-15 12:39:18 +0530

    ff

--- netlify/functions/updateProject.js
+++ netlify/functions/updateProject.js
@@ -5,13 +5,20 @@ const admin = require("firebase-admin");
 // Initialize Firebase Admin SDK if not already initialized
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    }),
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-  });
+         credential: admin.credential.cert({
+           "type": process.env.FB_TYPE,
+           "project_id": process.env.FB_PROJECT_ID,
+           "private_key_id": process.env.FB_PRIVATE_KEY_ID,
+           "private_key": process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n"),
+           "client_email": process.env.FB_CLIENT_EMAIL,
+           "client_id": process.env.FB_CLIENT_ID,
+           "auth_uri": process.env.FB_AUTH_URI,
+           "token_uri": process.env.FB_TOKEN_URI,
+           "auth_provider_x509_cert_url": process.env.FB_AUTH_PROVIDER_X509_CERT_URL,
+           "client_x509_cert_url": process.env.FB_CLIENT_X509_CERT_URL,
+         }),
+         // You don't need a databaseURL for Firestore unless you have a specific requirement.
+       });
 }
 
 const db = admin.firestore();
